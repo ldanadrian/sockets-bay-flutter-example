cfb717eb47d584947a45492168815787437554f7
Dan Adrian Lutescu <ldanadrian@gmail.com>
2023-02-02 11:54:55 +0200

    first commit with sockets bay code

--- sockets_bay_flutter_example/lib/main.dart
+++ sockets_bay_flutter_example/lib/main.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: unnecessary_brace_in_string_interps, avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/status.dart' as status;
 
 void main() {
   runApp(const MyApp());
@@ -30,12 +34,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final String socketsBayUrl = "wss://socketsbay.com/wss/v2/1/demo/";
 
-  void _incrementCounter() {
+  late WebSocketChannel channel;
+  bool isConnected = false;
+
+  List<String> messages = [];
+
+  _connect() {
+    try {
+      channel = WebSocketChannel.connect(
+        Uri.parse(socketsBayUrl),
+      );
+      channel.stream.listen(_onMessage, onError: _onError);
+      setState(() {
+        isConnected = true;
+      });
+
+      print("Connected with success!");
+    } on Exception catch (_) {
+      print("ERROR! Cannot connect to host!");
+    }
+  }
+
+  _sendMessage(String message) {
+    channel.sink.add(message);
     setState(() {
-      _counter++;
+      messages.add(message);
     });
+
+    print("We sent: ${message}");
+  }
+
+  _onMessage(dynamic message) {
+    setState(() {
+      messages.add(message);
+    });
+    print("We have received: ${message}");
+  }
+
+  _onError(Object err) {}
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
@@ -46,23 +88,30 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Colors.blue,
+                height: 200,
+                child: ListView.builder(
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      var item = messages[index];
+                      return ListTile(
+                          title: Text(item,
+                              style: const TextStyle(color: Colors.black)));
+                    }),
+              ),
+              isConnected
+                  ? const Text("connected")
+                  : ElevatedButton(
+                      onPressed: (() => _connect()),
+                      child: const Text("connect")),
+              ElevatedButton(
+                  onPressed: (() => _sendMessage("hello hello!")),
+                  child: const Text("send message")),
+            ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
